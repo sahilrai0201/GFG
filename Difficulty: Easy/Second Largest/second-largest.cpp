095edd315fca03a9c0c3c953660573cc3bd88763
Sahil Rai <sahilatwork21@gmail.com>
2026-02-18 21:48:36 +0530

    Optimize second largest finder to O(n) approach

--- Difficulty: Easy/Second Largest/second-largest.cpp
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -1,16 +1,43 @@
+//APPROACH-2 : BETTER - O(n)
 class Solution {
   public:
     int getSecondLargest(vector<int> &arr) {
-        sort(arr.begin(), arr.end());
         int n = arr.size();
         
-        int largest = arr[n-1];
+        int largest = -1;
+        int secondLargest = -1;
         
-        for(int i=n-2; i >= 0; i--){
-            if(arr[i] != largest){
-                return arr[i];
+        for(int i=0; i<n; i++){
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > secondLargest){
+                secondLargest = arr[i];
             }
         }
-        return -1;
+        return secondLargest;
     }
 };
+
+
+
+
+//APPROACH-1 : BRUTE FORCE - O(nlogn + n)
+// class Solution {
+//   public:
+//     int getSecondLargest(vector<int> &arr) {
+//         sort(arr.begin(), arr.end());
+//         int n = arr.size();
+        
+//         int largest = arr[n-1];
+        
+//         for(int i=n-2; i >= 0; i--){
+//             if(arr[i] != largest){
+//                 return arr[i];
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
