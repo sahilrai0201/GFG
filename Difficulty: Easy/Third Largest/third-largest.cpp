0140cde2687f674b7de43a07f98fab31a54c33ca
Sahil Rai <sahilatwork21@gmail.com>
2026-03-11 12:30:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Third Largest/third-largest.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int thirdLargest(vector<int> &arr) {
+        // code here
+        long long first = LLONG_MIN;
+        long long second = LLONG_MIN;
+        long long third = LLONG_MIN;
+        
+        for(int x : arr){
+            //skips duplicates
+            // if(x == first || x == second || x == third) continue;
+            
+            if(x > first){
+                third = second;
+                second = first;
+                first = x;
+            }
+            else if(x > second){
+                third = second;
+                second = x;
+            }
+            else if(x > third){
+                third = x;
+            }
+        }
+        return (third == LLONG_MIN) ? first : third;
+    }
+};
