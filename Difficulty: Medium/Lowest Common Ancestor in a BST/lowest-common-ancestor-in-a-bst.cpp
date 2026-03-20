19d033b3566196f48c3537d9e92e63e547982b53
Sahil Rai <sahilatwork21@gmail.com>
2026-03-20 18:33:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    Node* LCA(Node* root, Node* n1, Node* n2) {
+        //base case
+        if(root == NULL) return NULL;
+        
+        if(root->data < n1->data && root->data < n2->data){
+            return LCA(root->right, n1, n2);
+        }
+        
+        if(root->data > n1->data && root->data > n2->data){
+            return LCA(root->left, n1, n2);
+        }
+        
+        return root;
+    }
+};
