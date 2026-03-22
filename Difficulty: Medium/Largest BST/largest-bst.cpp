a64c6e2693a19696e6e98f2c66c3131987026386
Sahil Rai <sahilatwork21@gmail.com>
2026-03-22 13:41:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest BST/largest-bst.cpp
@@ -0,0 +1,49 @@
+class Solution {
+  public:
+  
+    class info{
+      public:
+        int maxi;
+        int mini;
+        bool isBST;
+        int size;
+    };
+    
+    info solve(Node* root, int &ans){
+        //base case
+        if(root == NULL){
+            return {INT_MIN, INT_MAX, true, 0};
+        }
+        
+        info left = solve(root->left, ans);
+        info right = solve(root->right, ans);
+        
+        info currNode;
+        
+        currNode.size = left.size + right.size + 1;
+        currNode.maxi = max(root->data, right.maxi);
+        currNode.mini = min(root->data, left.mini);
+        
+        if(left.isBST && right.isBST && 
+            (root->data > left.maxi && root->data < right.mini)){
+                
+            currNode.isBST = true;
+            ans = max(ans, currNode.size);
+        }
+        else{
+            currNode.isBST = false;
+            currNode.maxi = INT_MAX;
+            currNode.mini = INT_MIN;
+        }
+        
+        return currNode;
+    }
+    
+    /*You are required to complete this method */
+    // Return the size of the largest sub-tree which is also a BST
+    int largestBst(Node *root) {
+        int maxSize = 0;
+        solve(root, maxSize);
+        return maxSize;
+    }
+};
