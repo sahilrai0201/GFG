757920eb3a494f2de02d563d785166e4c3dd1398
Sahil Rai <sahilatwork21@gmail.com>
2026-04-17 14:39:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+    vector<int> findMajority(vector<int>& arr) {
+        // Code here
+        int n = arr.size();
+        
+        int candidate1 = 0, candidate2 = 1;
+        int count1 = 0, count2 = 0;
+        
+        //find potential candidates
+        for(int num : arr){
+            if(num == candidate1){
+                count1++;
+            }
+            else if(num == candidate2){
+                count2++;
+            }
+            else if(count1 == 0){
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                candidate2 = num;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        //verify candidate
+        count1 = 0;
+        count2 = 0;
+        
+        for(int num : arr){
+            if(num == candidate1) count1++;
+            else if(num == candidate2) count2++;
+        }
+        
+        vector<int> result;
+        if(count1 > n/3) result.push_back(candidate1);
+        if(count2 > n/3) result.push_back(candidate2);
+        
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
+
+
+
+
