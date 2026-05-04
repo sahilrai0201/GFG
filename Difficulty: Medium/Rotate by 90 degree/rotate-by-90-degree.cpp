a9cb4eafe51ca4e55b5a38100c7bdc16e2dc3e3f
Sahil Rai <sahilatwork21@gmail.com>
2026-05-04 23:39:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.cpp
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.cpp
@@ -4,12 +4,15 @@ class Solution {
         // code here
         int n = mat.size();
         
+        //transpose of the matrix
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 swap(mat[i][j], mat[j][i]);
             }
         }
         
+        //reverse the transposed matrix manually
+        //COLUMN WISE REVERSAL FOR ANTI-CLOCKWISE ROTATION----->
         for(int j=0; j<n; j++){
             int top = 0, bottom = n-1;
             while(top < bottom){
@@ -18,5 +21,8 @@ class Solution {
                 bottom--;
             }
         }
+        
     }
 };
+
+//TC = O(N*N) & SC = O(1)--------------------->
