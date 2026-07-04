6a217aadc21531514236e864c11aba0cb92bf1be
Sahil Rai <sahilatwork21@gmail.com>
2026-07-04 09:12:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+  public:
+    Node *reverse(Node *head) {
+        // code here
+        stack<int> st;
+        
+        Node* temp = head;
+        
+        while(temp != NULL){
+            st.push(temp->data);
+            temp = temp->next;
+        }
+        
+        temp = head;
+        
+        while(temp != NULL){
+            temp->data = st.top();
+            st.pop();
+            temp = temp->next;
+        }
+        
+        return head;
+    }
+};
